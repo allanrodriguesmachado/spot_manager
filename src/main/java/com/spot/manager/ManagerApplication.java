c1715b5776916058fdadcf0586aa4f5e3555fc9f
Consultoria 01 <consultoria01@shibata.corp.br>
2024-12-11 18:06:18 -0300

    Install dependenci Project

---
+++ src/main/java/com/spot/manager/ManagerApplication.java
@@ -0,0 +1,13 @@
+package com.spot.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("SPOT MANAGER");
+	}
+
+}
